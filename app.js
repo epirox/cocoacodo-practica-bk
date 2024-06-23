e6d0936a93564e859a09c0440ff41e0db3dcc909
Alvaro Leandro Garcia Vera <alvarolvgarcia@hotmail.com>
2024-06-23 19:04:03 -0300

    primera subida

---
+++ app.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require('fs')
+const path = require('path')
+
+
+let users = [
+    { id: 1, name: 'John Doe' },
+    { id: 2, name: 'Jane Smith' }
+];
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    const filePath = path.join(__dirname, 'tpl', 'index.html');
+
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error al leer el archivo:', err);
+            res.status(500).send('Error al leer el archivo');
+            return;
+        }
+
+        res.send(data);
+    });
+})
+
+app.get('/users', (req, res) => {
+    res.json(users);
+})
+
+app.get('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id))
+    if (!user) return res.status(404).send('User not found')
+    res.json(user)
+})
+
+app.post('/users', (req, res) => {
+    const user = {
+        id: users.length + 1,
+        name: req.body.name
+    };
+    users.push(user);
+    res.status(201).json(user);
+});
+
+app.put('/users/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if (!user) return res.status(404).send('User not found');
+    user.name = req.body.name;
+    res.json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+    const userIndex = users.findIndex(u => u.id === parseInt(req.params.id));
+    if (userIndex === -1) return res.status(404).send('User not found');
+    users.splice(userIndex, 1);
+    res.status(204).send();
+});
+app.listen(3000, () => {
+    console.log(`Servidor corriendo en puerto http://localhost:${port}/`)
+})
