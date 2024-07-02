d1dda173b5d15a659bc3bbe243a7fd0374389c82
Alvaro Leandro Garcia Vera <alvarolvgarcia@hotmail.com>
2024-07-02 18:02:52 -0300

    fix db script connect

--- db/db.js
+++ db/db.js
@@ -3,13 +3,13 @@ const mysql = require('mysql2')
 const DB_HOST = process.env.DB_HOST || 'localhost'
 const DB_USER = process.env.DB_USER || 'root'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'pass'
-const DB_DATABASE = process.env.DB_DATABASE || 'mangaProject'
+const DB_NAME = process.env.DB_NAME || 'mangaProject'
 
 const connection = mysql.createConnection({
     host : DB_HOST,
     user : DB_USER,
     password : DB_PASSWORD,
-    database : DB_DATABASE
+    database : DB_NAME
 })
 
 connection.connect((err) => {
