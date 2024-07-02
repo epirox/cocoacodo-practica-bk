553e6de98e7a6f136dd7c507e385243175e6f4f1
Alvaro Leandro Garcia Vera <alvarolvgarcia@hotmail.com>
2024-07-01 22:07:06 -0300

    mas correcciones

--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
-const fs = require('fs')
-const path = require('path')
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-const userRoutes = require("../routes/userRoutes")
+// Importa las rutas de usuarios desde userRoutes.js
+const userRoutes = require('./routes/userRoutes');
 
+// Middleware para procesar body de solicitudes
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-
+// Ruta de inicio que devuelve un archivo HTML
 app.get('/', (req, res) => {
     const filePath = path.join(__dirname, 'tpl', 'index.html');
 
@@ -24,10 +25,12 @@ app.get('/', (req, res) => {
 
         res.send(data);
     });
-})
+});
 
-app.use("/users", userRoutes)
+// Monta las rutas del usuario bajo el prefijo /users
+app.use('/users', userRoutes);
 
+// Inicia el servidor en el puerto especificado
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en puerto http://localhost:${PORT}/`)
-})
+    console.log(`Servidor corriendo en puerto http://localhost:${PORT}/`);
+});
