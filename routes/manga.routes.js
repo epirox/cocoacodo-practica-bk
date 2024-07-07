b0235aaa858fc51e91d43324e64b2f71ff0cc7fb
Alvaro Leandro Garcia Vera <alvarolvgarcia@hotmail.com>
2024-07-06 21:06:54 -0300

    add rout manga/upload/covers/:image

--- routes/manga.routes.js
+++ routes/manga.routes.js
@@ -1,14 +1,34 @@
-import express from 'express';
-import multer from 'multer';
-import MangaController from '../controllers/manga.controller.js';
+import express from 'express'
+import multer from 'multer'
+import MangaController from '../controllers/manga.controller.js'
+import { promises as fs } from 'fs'
+import { join, dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const router = express.Router();
-const upload = multer({ dest: 'uploads/covers' });
+const router = express.Router()
+const upload = multer({ dest: 'uploads/covers' })
 
-router.post('/', upload.single('cover'), (req, res) => MangaController.createManga(req, res));
-router.get('/', (req, res) => MangaController.getAllMangas(req, res));
-router.get('/:id', (req, res) => MangaController.getMangaById(req, res));
-router.put('/:id', upload.single('cover'), (req, res) => MangaController.updateManga(req, res));
-router.delete('/:id', (req, res) => MangaController.deleteManga(req, res));
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const uploadsDir = join(__dirname, '..', 'uploads', 'covers');
 
-export default router;
+router
+    .post('/', upload.single('cover'), (req, res) => MangaController.createManga(req, res))
+    .get('/', (req, res) => MangaController.getAllMangas(req, res))
+    .get('/:id', (req, res) => MangaController.getMangaById(req, res))
+    .put('/:id', upload.single('cover'), (req, res) => MangaController.updateManga(req, res))
+    .delete('/:id', (req, res) => MangaController.deleteManga(req, res)) 
+    .get('/uploads/covers/:image', (req, res) => {
+        const imageName = req.params.image
+        const imagePath = join(uploadsDir, imageName)
+
+        fs.access(imagePath, fs.constants.F_OK)
+            .then(() => {
+                res.setHeader('Content-Type', 'image/jpeg')
+                res.sendFile(imagePath)
+            })
+            .catch((err) => {
+                console.error('Error al acceder a la imagen:', err)
+                res.status(404).json({ error: 'Imagen no encontrada' })
+            })
+    })
+export default router
